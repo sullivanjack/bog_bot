51fadf1b2a80ec22cd77d9dd64818d4d438f3db4
Jack Sullivan <sullivanjack87@gmail.com>
2017-01-04 14:52:41 -0600

    WALL 3.0

--- bot.js
+++ bot.js
@@ -4,7 +4,7 @@ var botID = process.env.BOT_ID;
 
 function respond() {
 	var request = JSON.parse(this.req.chunks[0]),
-    console.log(process.versions);
+
 	// Commands Here:
 	weatherRegex = /^\/stan weather/;
 	wallRegex = /^/wall/;
