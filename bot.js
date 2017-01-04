6bae89b515c2680721d3763f11c6e162d2736ce8
Jack Sullivan <sullivanjack87@gmail.com>
2017-01-03 22:09:57 -0600

    i should find a better way to test this still

--- bot.js
+++ bot.js
@@ -41,38 +41,43 @@ function postMessage() {
 		botResponse = "'The forecast for: " + l[0].forecast[0].day + ", " + l[0].forecast[0].date + " is /n/n High/Low: "
 			+ l[0].forecast[0].high + "°F/" + l[0].forecast[0].low + "°F, " + l[0].forecast[0].skytextday + " with a " 
 			+ l[0].forecast[0].precip + " chance of precipatation'";
-	});
-	
+			
+			
 		console.log(botResponse);
   
-	  options = {
-		hostname: 'api.groupme.com',
-		path: '/v3/bots/post',
-		method: 'POST'
-	  };
+		options = {
+			hostname: 'api.groupme.com',
+			path: '/v3/bots/post',
+			method: 'POST'
+		};
 
-	  body = {
-		"bot_id" : botID,
-		"text" : botResponse
-	  };
+		body = {
+			"bot_id" : botID,
+			"text" : botResponse
+		};
 
-	  console.log('sending ' + botResponse + ' to ' + botID);
+		console.log('sending ' + botResponse + ' to ' + botID);
 
-	  botReq = HTTPS.request(options, function(res) {
-		  if(res.statusCode == 202) {
+		botReq = HTTPS.request(options, function(res) {
+			if(res.statusCode == 202) {
 			//neat
-		  } else {
+			} else {
 			console.log('rejecting bad status code ' + res.statusCode);
-		  }
-	  });
+			}
+		});
 
-	  botReq.on('error', function(err) {
-		console.log('error posting message '  + JSON.stringify(err));
-	  });
-	  botReq.on('timeout', function(err) {
-		console.log('timeout posting message '  + JSON.stringify(err));
-	  });
-	  botReq.end(JSON.stringify(body));
+		botReq.on('error', function(err) {
+			console.log('error posting message '  + JSON.stringify(err));
+		});
+		
+		botReq.on('timeout', function(err) {
+			console.log('timeout posting message '  + JSON.stringify(err));
+		});
+		
+		botReq.end(JSON.stringify(body));
+	});
+		
+	  
 }
 
 
